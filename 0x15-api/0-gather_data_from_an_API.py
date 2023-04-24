60849e84415617d8dbe81ad4f6023985c6b35045
sethdanny <naddulidaniel1994@gmail.com>
2023-04-24 14:53:02 +0300

    fix

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" make a request to a url and extract data """
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    emp_id = argv[1]
+    total_todos = 0
+    done_todos = 0
+    done_todo_titles = []
+
+    result = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                   emp_id)
+    emp_name = result.json().get('name')
+
+    result = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                   emp_id + '/todos')
+    emp_todos = result.json()
+
+    for item in emp_todos:
+        total_todos += 1
+
+        if item.get('completed') is True:
+            done_todos += 1
+            done_todo_titles.append(item.get('title'))
+
+    print('Employee {} is done with tasks({}/{}):'.format(
+                                                   emp_name,
+                                                   done_todos,
+                                                   total_todos
+                                                   ))
+
+    for title in done_todo_titles:
+        print('\t ' + title)
+    
