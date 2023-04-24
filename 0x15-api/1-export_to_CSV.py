ddcb17f659f9288743515ab5974bdc90e2c54724
sethdanny <naddulidaniel1994@gmail.com>
2023-04-24 15:52:11 +0300

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" convert json to csv file """
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    emp_id = argv[1]
+    file_name = emp_id + '.csv'
+    total_todos = 0
+    done_todos = 0
+    done_todos_titles = []
+
+    res = requests.get(
+                   'https://jsonplaceholder.typicode.com/users/' +
+                   emp_id)
+    emp_name = res.json().get('username')
+
+    res = requests.get(
+                   'https://jsonplaceholder.typicode.com/users/' +
+                   emp_id + '/todos')
+    emp_todos = res.json()
+
+    with open(file_name, 'w') as csvfile:
+        for todo in emp_todos:
+            total_todos += 1
+            csvfile.write(
+                    '"{}","{}","{}","{}"\n'.format(
+                                            todo.get('userId'),
+                                            emp_name,
+                                            todo.get('completed'),
+                                            todo.get('title')
+                                            ))
