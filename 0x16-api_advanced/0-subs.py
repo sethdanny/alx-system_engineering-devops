d4b16b297f6a75e282e08db84e7175f8de01c003
sethdanny <naddulidaniel1994@gmail.com>
2023-05-09 12:12:02 +0300

    fix f string

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 def number_of_subscribers(subreddit):
     """ function to fetch number of subscribers """
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-agent": "nadduli daniel"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
