f7d9b75f0e6040536e22b56479c2c48652401b86
sethdanny <naddulidaniel1994@gmail.com>
2023-05-10 12:31:20 +0300

    fix'

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,6 @@
 # fix file extension typo in the php file
-exec { 'replace phpp with php':
-  command  => 'sed -ie \'s/class-wp-locale.phpp/class-wp-locale.php/\' /var/www/html/wp-settings.php',
-  provider => shell
-}
 
-#restart apache2 service
-exec { 'restart apache2 service':
-  command  => 'service apache2 restart',
-  provider => shell
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
