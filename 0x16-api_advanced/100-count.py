e97c071d8d670144ea1b554a0c8ed206aceddc8d
sethdanny <naddulidaniel1994@gmail.com>
2023-05-17 21:36:03 +0300

    more changes

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,36 +1,21 @@
 #!/usr/bin/python3
-"""fetch data from reddit api"""
+""" fetch data from reddit api """
+
 import requests
 
-
-def count_words(subreddit, word_list, found_list=[], after=None):
-    """ Prints counts of given words found in hot posts of a given subreddit."""
+def count_words(subreddit, word_list):
+    """Returns counts of given words found in hot posts of a given subreddit."""
     headers = {'User-agent': 'nadduli daniel'}
-    posts = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
-                         .format(subreddit, after), headers=headers)
-    if after is None:
-        word_list = [word.lower() for word in word_list]
+    posts = requests.get('http://www.reddit.com/r/{}/hot.json'.format(subreddit), headers=headers)
 
     if posts.status_code == 200:
-        posts = posts.json()['data']
-        aft = posts['after']
-        posts = posts['children']
+        posts = posts.json()['data']['children']
+        result = {word.lower(): 0 for word in word_list}
         for post in posts:
             title = post['data']['title'].lower()
             for word in title.split(' '):
-                if word in word_list:
-                    found_list.append(word)
-        if aft is not None:
-            count_words(subreddit, word_list, found_list, aft)
-        else:
-            result = {}
-            for word in found_list:
-                if word.lower() in result.keys():
-                    result[word.lower()] += 1
-                else:
-                    result[word.lower()] = 1
-            for key, value in sorted(result.items(), key=lambda item: item[1],
-                                     reverse=True):
-                print('{}: {}'.format(key, value))
+                if word in result:
+                    result[word] += 1
+        return result
     else:
-        return
+        return None
